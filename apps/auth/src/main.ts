86efc2324a7fbfe92f9f91c0afb795a3915aa784
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-05-20 20:24:41 +0700

    update: change schema running main.ts into createMicroservice

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,21 @@
+import { AUTH } from '@app/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        protoPath: join(__dirname, '../auth.proto'),
+        package: AUTH,
+      },
+    },
+  );
+
+  await app.listen();
 }
 bootstrap();
