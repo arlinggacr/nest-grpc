3a5a6d86c469275690e287ed9c4186ce6c5e80e6
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-05-21 20:11:28 +0700

    update: change the directory name and open package for auth system, and directing to auth.proto

--- apps/api-gateway/src/users/users.module.ts
+++ apps/api-gateway/src/users/users.module.ts
@@ -1,6 +1,7 @@
 import { AUTH_PACKAGE_NAME } from '@app/common';
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AUTH_SERVICE } from './constant';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -11,7 +12,10 @@ import { UsersService } from './users.service';
       {
         name: AUTH_SERVICE,
         transport: Transport.GRPC,
-        options: { package: AUTH_PACKAGE_NAME },
+        options: {
+          protoPath: join(__dirname, '../auth.proto'),
+          package: AUTH_PACKAGE_NAME,
+        },
       },
     ]),
   ],
